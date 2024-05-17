08cfe39de61a506a16ffcaa50f5f430b797dbe8a
Jose Antonio Sanchez Molina <jasanchez.smartway@externos.avatel.es>
2024-05-17 08:51:49 +0200

    Ajuste Vista torneo

--- src/components/Torneos/TorneoCard.vue
+++ src/components/Torneos/TorneoCard.vue
@@ -1,7 +1,45 @@
 <template>
-  <div></div>
+  <v-card outlined class="torneo-card">
+    <v-card-title>
+      {{ torneo.Nombre_Torneo }}
+    </v-card-title>
+    <v-card-text>
+      <p>{{ torneo.Descripción_Torneo }}</p>
+    </v-card-text>
+    <v-card-text>
+      <p>FECHA: {{ formatDate(torneo.Fecha_Inicio_Torneo) }}</p>
+      <p>LUGAR: {{ torneo.Lugar_Torneo }}</p>
+      <p>RONDAS: {{ torneo.Numero_Partidas }}</p>
+      <p>PUNTOS: {{ torneo.Puntos_Torneo }}</p>
+      <p>PRECIO: {{ torneo.Precio_Torneo }}</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn variant="tonal" @click="goToDetalle">Ver Detalle</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Torneo } from "@/interfaces/Torneo";
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
 
-<style scoped></style>
+const props = defineProps<{ torneo: Torneo }>();
+const router = useRouter();
+
+const goToDetalle = () => {
+  router.push(`/detalle-torneo/${props.torneo.ID_Torneo}`);
+};
+const formatDate = (date: Date) => {
+  const options = { year: "numeric", month: "numeric", day: "numeric" };
+  return new Date(date).toLocaleDateString();
+};
+</script>
+
+<style scoped>
+.torneo-card {
+  max-width: 100%;
+  min-width: 300px;
+  margin: 16px;
+}
+</style>
--- src/views/TorneosView.vue
+++ src/views/TorneosView.vue
@@ -1,68 +1,28 @@
 <template>
   <div>
     <NavBar />
-    <div class="vista-torneos">
-      <div
-        class="tarjeta-torneo"
-        v-for="torneo in listaTorneos"
-        :key="torneo.ID_Torneo"
-      >
-        <!-- Contenido de la tarjeta del torneo -->
-        <h2>{{ torneo.Nombre_Torneo }}</h2>
-        <p>
-          FECHA: {{ torneo.Fecha_Inicio_Torneo.getDate() }}/{{
-            torneo.Fecha_Inicio_Torneo.getDay()
-          }}/{{ torneo.Fecha_Inicio_Torneo.getFullYear() }}
-        </p>
-        <p>LUGAR: {{ torneo.Lugar_Torneo }}</p>
-        <p>RONDAS: {{ torneo.Numero_Partidas }}</p>
-        <p>PRECIO: {{ torneo.Precio_Torneo }}</p>
-        <p>PUNTOS: {{ torneo.Puntos_Torneo }}</p>
-        <p>
-          <button @click="goToDetalle(torneo.ID_Torneo)" type="button">
-            Ver mas detalle
-          </button>
-        </p>
-
-        <!-- Otros detalles del torneo como fechas, precios, etc. -->
-      </div>
-    </div>
+    <v-container>
+      <v-row dense>
+        <v-col v-for="torneo in listaTorneos" :key="torneo.ID_Torneo">
+          <TorneoCard :torneo="torneo" />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
 import NavBar from "@/components/NavBar.vue";
 import { Torneo } from "@/interfaces/Torneo";
-import { useRouter } from "vue-router";
 import { getTorneosMock } from "@/services/TorneosService";
 import { onMounted, ref } from "vue";
+import TorneoCard from "@/components/Torneos/TorneoCard.vue";
 
 const listaTorneos = ref<Torneo[]>([]);
-const router = useRouter();
 
 onMounted(async () => {
   listaTorneos.value = await getTorneosMock();
 });
-
-const goToDetalle = (idTorneo: number) => {
-  router.push(`/detalle-torneo/${idTorneo}`);
-};
 </script>
 
-<style scoped>
-/* Estilos para la vista de torneos y las tarjetas */
-.vista-torneos {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-}
-
-.tarjeta-torneo {
-  width: 300px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 20px;
-  margin: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-</style>
+<style scoped></style>
