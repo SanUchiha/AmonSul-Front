56a79e1231ef6d6ce26a2d906f2eda196e69fdc1
Jose Antonio Sanchez Molina <jasanchez.smartway@externos.avatel.es>
2024-08-06 13:56:16 +0200

    fix a llamada mas rapida

--- src/services/UsuariosService.ts
+++ src/services/UsuariosService.ts
@@ -1,47 +1,47 @@
 import { EditarFaccionDTO } from "@/interfaces/Faccion";
 import { NewUserDTO } from "@/interfaces/Usuario";
-import { http } from './index'
+import { http } from "./index";
 
 class UsuariosService {
   static newUser = (newUser: NewUserDTO) => {
-    return http.post(`Usuario/Registrar`, newUser)
+    return http.post(`Usuario/Registrar`, newUser);
   };
 
   static getUsuarioPartidas = (email: string) => {
-    return http.get(`Usuario/Email/${email}`)
+    return http.get(`Usuario/Email/${email}`);
   };
 
   static getUsuario = (email: string) => {
-    return http.get(`Usuario/${email}`)
+    return http.get(`Usuario/${email}`);
   };
 
   static getUsuarios = () => {
-    return http.get(`Usuario/All`)
+    return http.get(`Usuario`);
   };
 
   static editarUsuario = (json: any) => {
-    return http.put(`Usuario/Editar`, json)
+    return http.put(`Usuario/Editar`, json);
   };
 
   static getNickById = (idUsuario: number) => {
-    return http.get(`Usuario/id/${idUsuario}`)
+    return http.get(`Usuario/id/${idUsuario}`);
   };
 
   static getUsuarioByNick = (nick: string) => {
-    return http.get(`Usuario/nick/${nick}`)
+    return http.get(`Usuario/nick/${nick}`);
   };
 
   static getDetalleUsuarioByEmail = (email: string) => {
-    return http.get(`Usuario/detalle/${email}`)
+    return http.get(`Usuario/detalle/${email}`);
   };
 
   static getUsuarioData = (idUsuario: number) => {
-    return http.get(`Usuario/Data/${idUsuario}`)
+    return http.get(`Usuario/Data/${idUsuario}`);
   };
 
   static editarFaccion = (body: EditarFaccionDTO) => {
-    return http.put(`Usuario/modificar-faccion`, body)
+    return http.put(`Usuario/modificar-faccion`, body);
   };
 }
 
-export { UsuariosService }
+export { UsuariosService };
